4624c567b02774df2d3cfac6e6c59965284c724e
Migueldp <migueldinizp@gmail.com>
2023-11-11 15:30:01 +0100

    [VISUAL] main layout updated, added menu, changed recycler item notification, changed strings.xml

--- java/emedp/notificationlogger/MainActivity.kt
+++ java/emedp/notificationlogger/MainActivity.kt
@@ -1,40 +1,90 @@
 package emedp.notificationlogger
 
 import android.Manifest
-import android.app.Activity
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.widget.Button
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import emedp.notificationlogger.database.Database
 import emedp.notificationlogger.database.NotificationAdapter
+import emedp.notificationlogger.database.NotificationDAO
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
+    private lateinit var dao: NotificationDAO
+    private lateinit var listApps: List<String>
+    private lateinit var listCategories: List<String>
+    private lateinit var notificationAdapter: NotificationAdapter
+    private lateinit var tvTotal: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        requestAppPermissions()
+        // UI
+        tvTotal = findViewById(R.id.tv_total)
+        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
+        val fabRefresh: FloatingActionButton = findViewById(R.id.fab_refresh)
+        val fabFilter: FloatingActionButton = findViewById(R.id.fab_filter)
 
+        fabFilter.setOnClickListener { filterUI() }
+        fabRefresh.setOnClickListener {
+            refreshUI()
+            Toast.makeText(this, getString(R.string.refreshed), Toast.LENGTH_SHORT).show()
+        }
+
+        // database
         val appDatabase = Database.getInstance(applicationContext)
-        val dao = appDatabase.notificationDAO()
-
-        // RecyclerView Adapter
-        val notifications = dao.allNotifications
-        val notificationAdapter = NotificationAdapter(notifications)
+        dao = appDatabase.notificationDAO()
+        val listNotifications = dao.allNotifications
 
         // RecyclerView
-        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
+        notificationAdapter = NotificationAdapter(listNotifications)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = notificationAdapter
 
-        val bOpenSettings: Button = findViewById(R.id.b_open_settings)
-        bOpenSettings.setOnClickListener { openNotificationListenerSettings() }
+        refreshUI()
+        requestAppPermissions()
 
-        val bClearAll: Button = findViewById(R.id.b_clear)
-        bClearAll.setOnClickListener {  } // TODO: put method to delete all
+        val firstTime = false
+        // TODO: si es la primera vez que se abre la app: (Comprobar con SharedPreferences) enseñar instrucciones
+        if (firstTime)
+            openNotificationListenerSettings()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.menu_delete_all -> {
+                alertDialogDelete()
+                true
+            }
+            R.id.menu_open_setting -> {
+                // TODO: abrir una nueva ventana que contenga todas las opciones necesarias para la escucha activa de notificaciones:
+                /*
+                TODO: ver como hace Mi Fit para ver lista de aplicaciones instaladas
+                - abrir ajuste de notification listener
+                - ahorro de energia
+                
+                 */
+                openNotificationListenerSettings()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     private fun requestAppPermissions() {
@@ -46,6 +96,50 @@ class MainActivity : Activity() {
     }
 
     private fun openNotificationListenerSettings () {
-        startActivity(Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS"))
+        // TODO: cambiar para que no sea un AlertDialog si no que todo esto se vea desde una pantalla
+        MaterialAlertDialogBuilder(this)
+            .setTitle(getString(R.string.setting_listener))
+            .setMessage(getString(R.string.setting_listener_desc))
+            .setNeutralButton(getString(R.string.cancel)) { dialog, _ -> dialog.dismiss() }
+            .setPositiveButton(getString(R.string.open_setting)) { _, _ -> startActivity(Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS")) }
+            .show()
+    }
+
+    private fun filterUI () {
+        val items = listApps.toTypedArray()
+        var itemChecked = 0
+
+        MaterialAlertDialogBuilder(this)
+            .setTitle(getString(R.string.filter_app))
+            .setSingleChoiceItems(items, itemChecked) { _, which -> itemChecked = which }
+            .setNeutralButton(getString(R.string.cancel)) { dialog, _ -> dialog.dismiss() }
+            .setNegativeButton(getString(R.string.clear)) { _, _ -> notificationAdapter.refresh(dao.allNotifications) }
+            .setPositiveButton(getString(R.string.apply)) { _, _ ->
+                notificationAdapter.refresh(dao.selectNotificationsWhereAppName(items[itemChecked]))
+                tvTotal.text = notificationAdapter.itemCount.toString()
+            }
+            .show()
+    }
+
+    private fun refreshUI () {
+        notificationAdapter.refresh(dao.allNotifications)
+        tvTotal.text = notificationAdapter.itemCount.toString()
+
+        listApps = dao.selectAppPackagesFromNotifications()
+        listCategories = dao.selectCategoriesFromNotifications()
+    }
+
+    private fun clearUI () {
+        dao.deleteNotificationsTable()
+        refreshUI()
+    }
+
+    private fun alertDialogDelete () {
+        MaterialAlertDialogBuilder(this)
+            .setTitle(getString(R.string.delete_all))
+            .setMessage(getString(R.string.delete_all_desc))
+            .setNeutralButton(getString(R.string.cancel)) { dialog, _ -> dialog.dismiss() }
+            .setPositiveButton(getString(R.string.delete_all)) { _, _ -> clearUI() }
+            .show()
     }
 }
