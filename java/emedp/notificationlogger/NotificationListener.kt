08edaf69ac5551582159e6e2728147f3ce8dbe84
Migueldp <migueldinizp@gmail.com>
2023-11-11 15:19:52 +0100

    [FEATURE] notification listener now stores in DB and not yet create notification

--- java/emedp/notificationlogger/NotificationListener.kt
+++ java/emedp/notificationlogger/NotificationListener.kt
@@ -1,75 +1,121 @@
 package emedp.notificationlogger
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.content.Context
 import android.icu.text.SimpleDateFormat
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
 import android.util.Log
-import androidx.core.app.NotificationCompat
+import android.widget.Toast
+import emedp.notificationlogger.database.Database
+import emedp.notificationlogger.database.Notification
 import java.util.Date
+import java.util.Locale
 
 class NotificationListener : NotificationListenerService() {
-    private lateinit var notificationManager: NotificationManager
-    private val channelID = "emedpware.notificationlogger.notification_channel.removed_notifications"
-    private var notificationID = 0
 
-    override fun onNotificationPosted(sbn: StatusBarNotification?) {
-        super.onNotificationPosted(sbn)
+    private val appDatabase = Database.getInstance(baseContext)
+
+    /**
+     * Implement this method to learn about when the listener is enabled and connected to
+     * the notification manager.  You are safe to call [.getActiveNotifications]
+     * at this time.
+     */
+    override fun onListenerConnected() {
+        super.onListenerConnected()
+        Log.d("NOTIFICATION_LISTENER","connected")
+        // TODO: string a xml
+        Toast.makeText(this, "Notification Listener Service connected", Toast.LENGTH_SHORT).show()
+        // TODO: cambiar por notificacion fijada
+        // TODO: Pasar a foregound service (?)
+        // TODO: copiar sistema de Mi Fitness para el reloj (inicio automático, etc)
+        //https://developer.android.com/reference/android/app/Service#startForeground(int,%20android.app.Notification,%20int)
     }
 
-    override fun onNotificationRemoved(sbn: StatusBarNotification?) {
-        super.onNotificationRemoved(sbn)
-        val notification = sbn!!.notification
-        val notificationWhen = SimpleDateFormat("dd/MM/yyyy hh:mm").format(Date(notification.`when`))
+    /**
+     * Implement this method to learn about when the listener is disconnected from the
+     * notification manager.You will not receive any events after this call, and may only
+     * call [.requestRebind] at this time.
+     */
+    override fun onListenerDisconnected() {
+        super.onListenerDisconnected()
+        Log.d("NOTIFICATION_LISTENER","disconnected")
+        Toast.makeText(this, "Notification Listener Service disconnected", Toast.LENGTH_SHORT).show()
+        // TODO: cambiar por notificacion fijada
+        // TODO: string a xml
+    }
+
+    override fun getActiveNotifications(): Array<StatusBarNotification> {
+        return super.getActiveNotifications()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("NOTIFICATION_LISTENER","DESTROY")
+    }
+
+    /**
+     * Implement this method to learn about new notifications as they are posted by apps.
+     *
+     * @param sbn A data structure encapsulating the original [android.app.Notification]
+     * object as well as its identifying information (tag and id) and source
+     * (package name).
+     */
+    override fun onNotificationPosted(sbn: StatusBarNotification?) {
+        super.onNotificationPosted(sbn)
+        Log.d("NOTIFICATION_LISTENER","NOTIFICATION POSTED")
+
+        val packageName = sbn?.packageName
+        val notification = sbn?.notification
+        val notificationWhen = SimpleDateFormat("dd/MM/yyyy hh:mm", Locale.getDefault()).format(
+            notification?.let { Date(it.`when`) })
         val notificationChannel = notification?.channelId.toString()
         val notificationCategory = notification?.category.toString()
+        val extras = notification?.extras
+
+        Log.d("PACKAGE_NAME", packageName.toString())
         Log.d("NOTIFICATION", notification.toString())
         Log.d("NOTIFICATION_WHEN", notificationWhen)
         Log.d("NOTIFICATION_CHANNEL", notificationChannel)
         Log.d("NOTIFICATION_CATEGORY", notificationCategory)
 
-        val extras = notification!!.extras
-        val title = extras.getString("android.title").toString()
-        val text = extras.getString("android.text").toString()
-        val subtext = extras.getString("android.subText").toString()
-        val textLines = extras.getString("android.textLines").toString()
+        val title = extras?.getString("android.title").toString()
+        val text = extras?.getString("android.text").toString()
+        val textLines = extras?.getCharSequence("android.textLines").toString()
         Log.d("EXTRAS",extras.toString())
         Log.d("EXTRAS_TITLE", title)
         Log.d("EXTRAS_TEXT", text)
         Log.d("EXTRAS_TEXT_LINES", textLines)
-        Log.d("EXTRAS_SUBTEXT", subtext)
 
-        if (notificationCategory == "msg") {
-            createNotificationChannel() // TODO: mejorar esta estructura para que no se llame todo el tiempo
-            when (textLines == "null") {
-                true -> showNotification("[$notificationWhen] $title", text)
-                false -> showNotification("[$notificationWhen] $title", "$text\n$textLines")
-            }
+        val notificationText = when (textLines == "null") {
+            true -> text
+            false -> textLines
         }
+
+        // Insert into DB
+        val notificationDB = Notification(packageName, notificationWhen, notificationChannel, notificationCategory, title, notificationText)
+        appDatabase.notificationDAO().insertNotification(notificationDB)
     }
 
-    private fun createNotificationChannel() {
-        val name = getString(R.string.notification_channel_name)
-        val desc = getString(R.string.notification_channel_desc)
-        val importance = NotificationManager.IMPORTANCE_DEFAULT
-        val channel = NotificationChannel(channelID, name, importance)
-        channel.description = desc
-
-        // Register the channel with the system
-        notificationManager = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.createNotificationChannel(channel)
-    }
-
-    private fun showNotification (title: String, message: String) {
-        val notification = NotificationCompat.Builder(this, channelID)
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .setSmallIcon(R.drawable.baseline_notifications_active_24)
-            .setContentTitle(title)
-            .setContentText(message)
-            .build()
-        notificationManager.notify(notificationID, notification)
-        notificationID++
+    /**
+     * Implement this method to learn when notifications are removed.
+     *
+     *
+     * This might occur because the user has dismissed the notification using system UI (or another
+     * notification listener) or because the app has withdrawn the notification.
+     *
+     *
+     * NOTE: The [StatusBarNotification] object you receive will be "light"; that is, the
+     * result from [StatusBarNotification.getNotification] may be missing some heavyweight
+     * fields such as [android.app.Notification.contentView] and
+     * [android.app.Notification.largeIcon]. However, all other fields on
+     * [StatusBarNotification], sufficient to match this call with a prior call to
+     * [.onNotificationPosted], will be intact.
+     *
+     * @param sbn A data structure encapsulating at least the original information (tag and id)
+     * and source (package name) used to post the [android.app.Notification] that
+     * was just removed.
+     */
+    override fun onNotificationRemoved(sbn: StatusBarNotification?) {
+        super.onNotificationRemoved(sbn)
+        Log.d("NOTIFICATION_LISTENER","NOTIFICATION REMOVED")
     }
 }
